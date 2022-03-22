03e5092ef96eabc2fe2535cd7b802572f4b72ebd
W4LDEMAR <sw_sosa@yahoo.com.mx>
2022-03-21 19:44:38 -0600

    Estructuras

--- Ahorcado/Proyecto 4.c
+++ Ahorcado/Proyecto 4.c
@@ -1,67 +1,175 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
-#define maxpal 20
-#define maxlet 15
+typedef struct {
+    char palabras [2][50];
+    int intentos;
+    char nombre[50];
+    int ranking;
+} Jugador;
 
-void impresionTurno(char);
-int buscarLetra(char, char), i, x;
-int turno(char);
 
-struct Jugador{
+void impresiondeturno (Jugador);
+Jugador crearjugador ();
+int buscarletra(char[],char);
+Jugador turno(Jugador,int *);
+int destaparLetras(char [][50],char);
 
-    int vidas;
-    char nombre[20];
-    char palabra[20];
-    char progreso[20];
-
-};
-
-int main(){
-
-    char palabras[maxpal][maxlet] = {"programa", "dinosaurio", "telefono", "ingenuo", "refrigerador", "renacuajo", "mantenimiento",
-    "resultados", "coronavirus", "alpinismo", "caballo", "hermano", "contaminar", "hormonas", "columpio", "ambulancia", "hormiguero",
-    "imprimir", "nostalgia", "literatura"};
-    int n_jug;
-
-    printf("\t\t A H O R C A D O\n");
-    n_jug = n_jugadores();
-
+int main()
+{
+    Jugador j[5];
+    srand(time(NULL));
+    int ganador=0,numJ,i;
+    int fin=0,maxR,minR,cont;
+    ///PREGUNTAMOS CUANTOS JUGADORES
+    do{
+        printf("\nEscibe numero de jugadores");
+        scanf("%d",&numJ);
+        if(numJ<2||numJ>5){
+            printf("\nSolamente pueden ser entre 2 y 5 jugador");
+        }
+    }while(numJ<2||numJ>5);
+    minR=numJ;
+    maxR=1;
+    ///CREAMOS JUGADORES
+    for(i=0;i<numJ;i++){
+        j[i]=crearjugador();
+    }
+    while(fin==0){
+        for(i=0;i<numJ;i++){
+            if(j[i].ranking==0){
+                j[i]=turno(j[i],&ganador);
+                if(ganador==1){
+                    j[i].ranking=maxR;
+                    maxR++;
+                }
+                else{
+                    if(ganador==-1){
+                        j[i].ranking=minR;
+                        minR--;
+                    }
+                }
+            }
+        }
+        cont=0;
+        for(i=0;i<numJ;i++){
+            if(j[i].ranking==0){
+                cont++;
+            }
+        }
+        if(cont<=1){
+            fin=1;
+        }
+    }
+    printf("\nTermino el juego");
+    for(i=0;i<numJ;i++){
+        impresiondeturno(j[i]);
+    }
     return 0;
 }
-
-int n_jugadores(){
-
-    int n_jug;
-
-    printf("¿Cuantas personas jugaran?\n\t**De 1 a 5 jugadores**\n");
+Jugador turno(Jugador j,int* ganador){
+    char letra;
+    *ganador=0;///ganador=1 tenemos ganador,
+    ///ganador=0 sigue jugando
+    ///ganador=-1 perdio
+    ///Imprimir tablero
+    impresiondeturno(j);
+    ///Preguntar letra
+    printf("\nEscribe una letra:");
     fflush(stdin);
-    scanf("%d", &n_jug);
-
-    if(n_jug < 0 || n_jug > 5){
-        printf("introduce un numero de jugadores valido");
-        n_jug = n_jugadores();
+    scanf("%c",&letra);
+    ///Validar letra
+    if(buscarletra(j.palabras[0],letra)==1){
+        ///Se encontro la letra
+        ///Modiciaciones
+           ///Cambiar los guiones para que aparezcan letras
+           ///Validar ganador
+           if(destaparLetras(j.palabras,letra)==0){
+                ///Tenemos un ganador
+                *ganador=1;
+           }
+           /*else{
+                ///Se pasa al siguiente turno
+           }*/
     }
-
-    return n_jug;
+    else{
+        ///No se encontro la letra
+        ///Modificaciones
+           ///Reducir intentos
+        j.intentos--;
+           ///Validar perdedor
+        if(j.intentos<=0){
+            *ganador=-1;
+        }
+    }
+    impresiondeturno(j);
+    printf("\n");
+    system("pause");
+    return j;
 }
 
-struct Jugador crea_jug(char palabras[][maxlet]){
-
-    struct Jugador jug1;
-    int x = rand()%maxpal, n_letras, i;
-
-    printf("Insterte el nombre del nuevo jugador:\n");
-    fflush(stdin);
-    gets(&jug1.nombre);
-
-    n_letras = strlen(palabras[x]);
-    jug1.palabra = palabras[x];
-
-    for(i = 0; i < n_letras; i++){
-        jug1.progreso[i] = "_ ";
+int destaparLetras(char a[][50],char l){
+    ///regresa 0 si no hay guiones
+    ///regresa !=0 si hay guiones
+    int i,c=0;
+    for(i=0;i<strlen(a[0]);i++){
+        if(l==a[0][i]){
+            a[1][i]=l;
+        }
     }
+    for(i=0;i<strlen(a[1]);i++){
+        if(a[1][i]=='_'){
+            c++;
+        }
+    }
+    return c;
+}
 
-    return jug1;
+void impresiondeturno(Jugador j){
+    system("cls");
+    printf("\nAsignada:%s",j.palabras[0]);
+    printf("\nPalabra:%s",j.palabras[1]);
+    printf("\nIntentos:%d",j.intentos);
+    printf("\nNombre:%s",j.nombre);
+    if (j.ranking==0){
+        printf("\nRanking:????");
+    }
+    else {
+        printf("\nRanking:%d",j.ranking);
+    }
+}
+
+ Jugador crearjugador(){
+    Jugador nuevo;
+    char pals[5][50];
+    int x=0;
+    nuevo.ranking=0;
+    nuevo.intentos=3;
+    printf("\nEscribe tu id:");
+    fflush(stdin);
+    gets(nuevo.nombre);
+    strcpy(pals[x++],"CAYDE");
+    strcpy(pals[x++],"ZAVALA");
+    strcpy(pals[x++],"IKORA");
+    strcpy(pals[x++],"ERIS");
+    strcpy(pals[x++],"ORYX");
+    x=rand()%5;
+    strcpy(nuevo.palabras[0],pals[x]);
+    for (x=0;x<strlen(nuevo.palabras[0]);x++){
+       nuevo.palabras[1][x]='_';
+    }
+    nuevo.palabras[1][x]=NULL;
+    return nuevo;
+}
+
+int buscarletra(char a[],char l){
+    int i,r=0;
+    for (i=0;i<strlen(a) && r==0;i++){
+        if (l==a[i]){
+            r=1;
+        }
+    }
+    return r;
 }
---
+++ Estructuras/Practica 6.c
@@ -0,0 +1,223 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct{
+    char marca;
+    char modelo;
+    int ano;
+    float motor;
+    char placa;
+    int n_puertas;
+    int n_pasajeros;
+    float precio;
+} Automovil;
+
+typedef struct{
+    char id[7];
+    int numAut;
+    Automovil autos[5];
+    float precio;
+    int estado;
+} Embarque;
+
+int main(){
+    Embarque E[5];
+    int numE;
+
+}
+
+Automovil nuevoAuto(int n){
+
+    Automovil nuevo;
+
+    system("cls");
+    printf("\t *** AUTOMOVIL NUMERO %d ***", n);
+    printf("Introduce la marca del auto: \n");
+    fflush(stdin);
+    gets(nuevo.marca);
+    printf("Introduce el modelo del auto: \n");
+    fflush(stdin);
+    gets(nuevo.modelo);
+    printf("Introduce el ano del auto: \n");
+    fflush(stdin);
+    scanf("%d", &nuevo.ano);
+    printf("Introduce la placa del auto: \n");
+    fflush(stdin);
+    gets(nuevo.placa);
+    printf("Introduce la capacidad del motor: \n");
+    fflush(stdin);
+    scanf("%f", &nuevo.motor);
+    printf("Indica el numero de puertas que tiene el auto: \n");
+    fflush(stdin);
+    scanf("%d", &nuevo.n_puertas);
+    printf("Indica la cantidad de pasajeros maximos del auto: \n");
+    fflush(stdin);
+    scanf("%d", &nuevo.n_pasajeros);
+    printf("Introduce el precio del auto: \n");
+    fflush(stdin);
+    scanf("%f", &nuevo.precio);
+
+    return nuevo;
+}
+
+Embarque nuevoEmbarque(){
+
+    Embarque nuevo;
+    int i;
+
+    system("cls");
+    printf("Escribe el id del nuevo embarque:\n");
+    fflush(stdin);
+    gets(nuevo.id);
+
+    do{
+        printf("Indica el numero de autos que tendra el embarque:\n");
+        fflush(stdin);
+        scanf("d", &nuevo.numAut);
+    }while(comp_nAutos(nuevo.numAut) =! 1);
+
+    nuevo.precio = 0;
+
+    for(i = 0; i > nuevo.numAut; i++){
+        nuevo.autos[i] = nuevoAuto(i + 1);
+        nuevo.precio = nuevo.autos[i].precio + nuevo.precio;
+    }
+
+    do{
+        printf("Elige el estado del embarque:\n");
+        printf(" 1.- Pendiente\n 2.- En transito\n 3.- Entregado\n 4.- Cancelado\n");
+        fflush(stdin);
+        scanf("%d", &nuevo.estado);
+    }while(comp_estado(nuevo.estado) != 1);
+
+    return nuevo;
+}
+
+int comp_estado(int n){
+    if(n > 4 || n < 1){
+        printf("ERROR: Selecciona una opcion del menu\n")
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+int comp_nAutos(int n){
+    if(n > 5 || n < 1){
+        printf("ERROR: El embarque debe de tener entre 1 y 5 autos unicamente\n");
+        return 0;
+    }else{
+        return 1;
+    }
+}
+
+void impResumEmbarque(Embarque E){
+
+    system("cls");
+    printf("\n \t*** RESUMEN DEL EMBARQUE %s ***\n", E.id);
+    printf("El embarque cuenta con: %d autos \n", E.numAut);
+    printf("El precio total del embarque es de: %f \n", E.precio);
+    printf("El embarque se encuentra ---> ");
+    impEstado(E.estado);
+    system("pause");
+
+}
+
+void impEmbarque(Embarque E){
+
+    int i;
+
+    system("cls");
+    printf("\n\t*** DATOS DEL EMBARQUE %s ***\n", E.id);
+    printf("El embarque cuenta con: %d autos \n", E.numAut);
+    printf("Los autos son:\n");
+    for(i = 0; i < E.numAut; i++){
+        printf("\t %s %s \n",E.autos[i].marca, E.autos[i].modelo);
+    }
+    printf("El precio total del embarque es de: %f \n", E.precio);
+    printf("El embarque se encuentra ---> ");
+    impEstado(E.estado);
+    system("pause");
+
+}
+
+void impEstado(int estado){
+
+    switch(estado){
+        case 1:
+            printf("PENDIENTE");
+            break;
+        case 2:
+            printf("EN TRANSITO");
+            break;
+        case 3:
+            printf("ENTREGADO");
+            break;
+        case 4:
+            printf("CANCELADO");
+            break;
+    }
+}
+
+void impAuto(Embarque E, int n){
+
+    system("cls");
+    printf("\t *** DATOS DEL AUTO %d DENTRO DEL EMBARQUE %s ***\n\n", n, E.id);
+    printf("Marca del automovil: %s\n", E.autos[n].marca);
+    printf("Modelo del automovil: %s\n", E.autos[n].modelo);
+    printf("Ano del automovil: %s\n", E.autos[n].ano);
+    printf("Motor del automovil: %f\n", E.autos[n].motor);
+    printf("Placa del automovil: %s\n", E.autos[n].placa);
+    printf("Numero de puertas del automovil: %d\n", E.autos[n].n_puertas);
+    printf("Numero de pasajeros del automovil: %d\n", E.autos[n].n_pasajeros);
+    printf("Precio del automovil: %f\n", E.autos[n].precio);
+    system("pause");
+
+}
+
+void modDatosE(Embarque *E){
+    /// Recuerda hacer una comprobacion por si no esl embarque que desea modificar en el main
+    int opcion, numero;
+
+    system("cls");
+    do{
+        printf("\n\t Elige el campo que deseas modificar:\n");
+        printf(" 1.- ID del embarque.\n 2.- Estado del embarque.\n 3.- Autos del embarque\n");
+        printf(" 4.- Eliminar el embarque completo.\n");
+        printf("\t 5.- CANCELAR\n");
+        fflush(stdin);
+        scanf("%d", &opcion);
+    }while(comp_OpMod(opcion) != 1);
+
+    switch(opcion){
+        case 1:
+            printf("Introduce el nuevo id del embarque: \n");
+            fflush(stdin);
+            gets(*E.id);
+            break;
+        case 2:
+            do{
+                printf("Elige el nuevo estado del embarque:\n");
+                printf(" 1.- Pendiente\n 2.- En transito\n 3.- Entregado\n 4.- Cancelado\n");
+                fflush(stdin);
+                scanf("%d", &numero);
+                *E.estado = numero;
+            }while(comp_estado(nuevo.estado) != 1);
+            break;
+        case 3:
+            printf("Elige la opcion deseada:\n");
+            printf(" 1.- Modificar los datos de un auto del embarque. \n 2.- ELIMINAR un auto del embarque");
+            fflush("stdin");
+            /// Continua
+    }
+}
+
+int comp_OpMod(int n){
+    if(n >= 1 && n <= 5){
+        return 1;
+    }else{
+        printf("Introduce una opcion valida.");
+        return 0;
+    }
+}
